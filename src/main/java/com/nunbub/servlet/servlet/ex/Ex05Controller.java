ed7e50f04bfaadb2c736ded37aedc88ba9221f90
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-07-21 18:45:17 +0900

    [류지수] - post method 예제 기본 클래스 틀 추가

---
+++ src/main/java/com/nunbub/servlet/servlet/ex/Ex05Controller.java
@@ -0,0 +1,24 @@
+package com.nunbub.servlet.servlet.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Controller extends HttpServlet{
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		
+		
+		
+		
+	}
+}
